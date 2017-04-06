348b8484588a0778b9766009a0bf931462f72d36
Romil Chauhan <romilhc@gmail.com>
2017-04-06 18:01:27 -0400

    Connected to MySQL DB and started with development

---
+++ BarterSystem/static/js/script.js
@@ -0,0 +1,3 @@
+/**
+ * Created by Romil Chauhan on 4/6/2017.
+ */
--- BarterSystem/views.py
+++ BarterSystem/views.py
@@ -4,6 +4,8 @@ from django.template import RequestContext
 import json
 from django.views.decorators.csrf import csrf_protect, csrf_exempt
 from django.views.decorators.cache import cache_page
+from BarterSystem.models import *
+
 
 # Create your views here.
 def index(request):
@@ -11,11 +13,45 @@ def index(request):
 
 @csrf_exempt
 def signin(request):
+    if request.POST.get('fname'):
+        print(request.POST)
+        fname = request.POST.get('fname')
+        lname = request.POST.get('lname')
+        email = request.POST.get('email')
+        age = request.POST.get('age')
+        gender = request.POST.get('gender')
+        street = request.POST.get('street')
+        city = request.POST.get('city')
+        state = request.POST.get('state')
+        country = request.POST.get('country')
+        zipcode = request.POST.get('zipcode')
+        contact = request.POST.get('contact')
+        if request.POST.get('profession'):
+            profession = request.POST.get('profession')
+        else:
+            profession = None
+        password = request.POST.get('password')
+        user = User(fname=fname,lname=lname,email=email,age=age,gender=gender,street=street,city=city,state=state,country=country,zipcode=zipcode,contact=contact,profession=profession,password=password)
+        user.save()
+
     return render(request,'signin.html')
 
 @csrf_exempt
 def signup(request):
     return render(request,'signup.html')
 
+@csrf_exempt
 def home(request):
-    return render(request,'homepage.html')
+    print(request.POST)
+    if request.POST.get('email'):
+        try:
+            print("Password matched!!")
+            user = User.objects.get(email=request.POST.get('email'))
+            password = User.objects.get(password=request.POST.get('password'))
+            return render(request, 'homepage.html')
+        except:
+            print("Password not matching")
+            return render(request,'index.html')
+    else:
+        print("Incorrect")
+        return render(request,'index.html')
