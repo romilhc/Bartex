bc28d20b5f2b66a3ce53fce3891285cfbfb656f1
Romil Chauhan <romilhc@gmail.com>
2017-04-10 20:59:14 -0400

    Updated JS file with code for data transfer

--- BarterSystem/static/js/script.js
+++ BarterSystem/static/js/script.js
@@ -1,3 +1,62 @@
 /**
  * Created by Romil Chauhan on 4/6/2017.
  */
+function getCookie(name) {
+    var cookieValue = null;
+    if (document.cookie && document.cookie != '') {
+        var cookies = document.cookie.split(';');
+        for (var i = 0; i < cookies.length; i++) {
+            var cookie = jQuery.trim(cookies[i]);
+            // Does this cookie string begin with the name we want?
+            if (cookie.substring(0, name.length + 1) == (name + '=')) {
+                cookieValue = decodeURIComponent(cookie.substring(name.length + 1));
+                break;
+            }
+        }
+    }
+    return cookieValue;
+}
+$.ajaxSetup({
+    beforeSend: function(xhr, settings) {
+        if (!(/^http:.*/.test(settings.url) || /^https:.*/.test(settings.url))) {
+            // Only send the token to relative URLs i.e. locally.
+            xhr.setRequestHeader("X-CSRFToken", getCookie('csrftoken'));
+        }
+    }
+});
+
+
+function signin() {
+    alert("setup works");
+    var email = $('#email').val();
+    var password = $('#password').val();
+    var signinData = {'email': email, 'password': password};
+
+    alert(email);
+    alert(password);
+    $.ajax({
+        type: "POST",
+        url: "/homepage/",
+        data: signinData,
+        dataType: 'json',
+        "beforeSend": function(xhr, settings) {
+            console.log("Before Send");
+            $.ajaxSettings.beforeSend(xhr, settings);
+            alert("Sending");
+        },
+        success: function(result){
+            status = result.status;
+            session_id = result.session_id;
+            if(status=="success") {
+                alert("Success");
+                alert(status);
+            }
+            else {
+                alert(status);
+                location.reload();
+            }
+
+        }
+    });
+
+}
